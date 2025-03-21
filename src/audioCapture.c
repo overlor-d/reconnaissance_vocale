6011e126d67d38125de54f2298090e9b4b88bb8f
over <jeanlepeltier1@gmail.com>
2025-03-21 01:22:42 +0100

    Ajout d'un système de capture et d'enregistrement dans un fichier en binaire sous forme de structure pour chaque point d'échantillonage

--- src/audioCapture.c
+++ src/audioCapture.c
@@ -2,18 +2,118 @@
 #include <mmsystem.h>
 #include <stdio.h>
 
-
-#define ENCHANTILLONAGE 44100
-#define BITS_PAR_SAMPLE 16
+#include "audioCapture.h"
 
 
-int main(void)
+// Fonction pour enregistrer les données audio dans un fichier
+int enregistrement_fichier(short *buffer)
+{
+    int i;
+    FILE *fichierTestEnregistrement;
+    POINT_GRAPH point;
+
+    printf("Commencement de l'enregistrement dans le fichier de test\n");
+
+    fichierTestEnregistrement = fopen(FICHIER_TEST_ENREGISTREMENT, "wb");
+    if (fichierTestEnregistrement == NULL)
+    {
+        printf("Erreur lors de l'ouverture du fichier de test\n");
+        return 3;
+    }
+
+    for (i = 0; i < TAILLE_BUFFER / 2; i++)
+    {
+        point.valeur = buffer[i];
+        point.id = i + 1;
+
+        fwrite(&point, sizeof(POINT_GRAPH), 1, fichierTestEnregistrement);
+    }
+
+    fclose(fichierTestEnregistrement);
+    return 0;
+}
+
+int lecture_fichier()
+{
+    FILE *fichierTestEnregistrement;
+    POINT_GRAPH point;
+
+    printf("Lecture du fichier :\n");
+
+    fichierTestEnregistrement = fopen(FICHIER_TEST_ENREGISTREMENT, "rb");
+    if (fichierTestEnregistrement == NULL)
+    {
+        printf("Erreur lors de l'ouverture du fichier de test\n");
+        return 3;
+    }
+
+    while (fread(&point, sizeof(POINT_GRAPH), 1, fichierTestEnregistrement) == 1)
+    {
+        printf("Point num %d -> %hd\n", point.id, point.valeur);
+    }
+
+    fclose(fichierTestEnregistrement);
+    return 0;
+}
+
+// Fonction pour capturer le son depuis le micro
+int capture_audio(short *bufferSonBrut)
 {
     HWAVEIN handlePeriphAudio;
     WAVEFORMATEX formatAduio;
-
     MMRESULT statusPeriphOpen;
     MMRESULT statusPeriphClose;
+    WAVEHDR bufferDescriptor;
+
+    formatAduio.wFormatTag = WAVE_FORMAT_PCM;
+    formatAduio.nChannels = 1;
+    formatAduio.nSamplesPerSec = ENCHANTILLONAGE;
+    formatAduio.wBitsPerSample = BITS_PAR_SAMPLE;
+    formatAduio.nBlockAlign = (formatAduio.nChannels * formatAduio.wBitsPerSample) / 8;
+    formatAduio.nAvgBytesPerSec = formatAduio.nSamplesPerSec * formatAduio.nBlockAlign;
+    formatAduio.cbSize = 0;
+
+    statusPeriphOpen = waveInOpen(&handlePeriphAudio, WAVE_MAPPER, &formatAduio, 0, 0, CALLBACK_NULL);
+    if (statusPeriphOpen != MMSYSERR_NOERROR)
+    {
+        printf("Erreur lors de l'ouverture du périphérique. Erreur : %d\n", statusPeriphOpen);
+        return 1;
+    }
+
+    bufferDescriptor.lpData = (LPSTR)bufferSonBrut;
+    bufferDescriptor.dwBufferLength = TAILLE_BUFFER;
+    bufferDescriptor.dwBytesRecorded = 0;
+    bufferDescriptor.dwUser = 0;
+    bufferDescriptor.dwFlags = 0;
+    bufferDescriptor.dwLoops = 0;
+
+    waveInPrepareHeader(handlePeriphAudio, &bufferDescriptor, sizeof(WAVEHDR));
+    waveInAddBuffer(handlePeriphAudio, &bufferDescriptor, sizeof(WAVEHDR));
+
+    waveInStart(handlePeriphAudio);
+
+    while (!(bufferDescriptor.dwFlags & WHDR_DONE))
+    {
+        Sleep(10);
+    }
+
+    printf("Enregistrement terminé !\n");
+
+    waveInStop(handlePeriphAudio);
+    waveInUnprepareHeader(handlePeriphAudio, &bufferDescriptor, sizeof(WAVEHDR));
+    statusPeriphClose = waveInClose(handlePeriphAudio);
+
+    if (statusPeriphClose != MMSYSERR_NOERROR)
+    {
+        printf("Erreur lors de la fermeture du périphérique. Erreur : %d\n", statusPeriphClose);
+        return 2;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
 
     /* changement de l'encodage pour l'affichage de la console */
     UINT oldCP = GetConsoleOutputCP();
@@ -21,35 +121,15 @@ int main(void)
         SetConsoleOutputCP(65001);
     }
 
-    formatAduio.wFormatTag = WAVE_FORMAT_PCM;
-    formatAduio.nChannels = 1;
-    formatAduio.nSamplesPerSec = 44100;
-    formatAduio.wBitsPerSample = 16;
-    formatAduio.nBlockAlign = (formatAduio.nChannels * formatAduio.wBitsPerSample) / 8;
-    formatAduio.nAvgBytesPerSec = formatAduio.nSamplesPerSec * formatAduio.nBlockAlign;
-    formatAduio.cbSize = 0;
+    short bufferSonBrut[TAILLE_BUFFER];
 
+    capture_audio(bufferSonBrut);
 
+    enregistrement_fichier(bufferSonBrut);
 
-    statusPeriphOpen = waveInOpen(&handlePeriphAudio, WAVE_MAPPER, &formatAduio, 0, 0, CALLBACK_NULL);
+    printf("Échantillons sauvegardés dans %s\n", FICHIER_TEST_ENREGISTREMENT);
 
-    if (statusPeriphOpen != MMSYSERR_NOERROR)
-    {
-        printf("erreur lors de l'ouverture du périphérique. Erreur : %d\n", statusPeriphOpen);
-        return 1;
-    }
-
-    printf("Périphérique ouvert sans problèmes\n");
-
-    statusPeriphClose = waveInClose(handlePeriphAudio);
-
-    if (statusPeriphClose != MMSYSERR_NOERROR)
-    {
-        printf("erreur lors de la fermeture du périphérique. Erreur : %d\n", statusPeriphClose);
-        return 2;
-    }
-
-    printf("Périphérique fermé sans problèmes\n");
+    lecture_fichier();
 
     return 0;
 }
--- src/audioCapture.h
+++ src/audioCapture.h
@@ -1,8 +1,20 @@
-#ifndef AUDIO_CAPTURE
-#define AUDIO_CAPTURE
+#ifndef AUDIO_CAPTURE_H
+#define AUDIO_CAPTURE_H
+
+#define ENCHANTILLONAGE 44100
+#define BITS_PAR_SAMPLE 16
+#define TAILLE_BUFFER (ENCHANTILLONAGE * (BITS_PAR_SAMPLE / 8))
+#define T_NORMAL_WORD 50
+
+#define FICHIER_TEST_ENREGISTREMENT "./data/enchantillon"
+
+typedef struct {
+    int id;
+    short valeur;
+} POINT_GRAPH;
+
+int enregistrement_fichier(short *buffer);
+int capture_audio(short *bufferSonBrut);
 
 
-
-
-
-#endif /*AUDIO_CAPTURE*/
+#endif /*AUDIO_CAPTURE_H*/
